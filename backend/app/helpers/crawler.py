fe621cd20f7a2d824915d4b55abe06fc5ca0c4b5
milanchheta <milanchheta@gmail.com>
2020-11-13 01:13:10 -0500

    researchers data collection done

--- backend/app/helpers/crawler.py
+++ backend/app/helpers/crawler.py
@@ -1,8 +1,8 @@
 from scholarly import scholarly
 import requests
-# from bs4 import BeautifulSoup
 from flask import Response
 from tokenizor import populate_keyword
+import json
 
 def getAurthorObj(name):
     search_query = scholarly.search_author(name)
@@ -28,26 +28,15 @@ def getAllAbstract(pubList):
             pass
     return res
 
-
-# def getAbstract(url):
-#     page = requests.get(url)
-#     soup = BeautifulSoup(page.content, 'html.parser')
-#     results = soup.find('div', class_='abstractSection').get_text()
-#     ##add nlp algo here to extract imp words
-#     return results
-
-# author=getAurthorObj("Patrick Shih")
-# pubList=getPublicationList(author)
-# interests=getAuthorInterests(author)
-# absList=getAllAbstract(pubList)
-
 if __name__ == "__main__":
-    with open('researcher_list.txt', 'r') as f:
-        for name in f:
-            author = getAurthorObj(name)
+    with open('researchersList.json', 'r') as f:
+        data=json.load(f)
+    for department in data:
+        for entry in data[department]:
+            author = getAurthorObj(entry["name"])
             pub_list = getPublicationList(author)
             abs_val = getAllAbstract(pub_list)
             for key in abs_val:
                 keywords = populate_keyword(abs_val[key])
-                print(key, keywords)
-        #TODO: update mongo DB with title, keyword list for prof list.
+
+        # TODO: update mongo DB with title, keyword list for prof list.
