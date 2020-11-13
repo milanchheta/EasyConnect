55db8413efdbf13d9ef7abe2eb658d947c89feee
Goutham Gopal <goutham.gopal94@gmail.com>
2020-11-13 02:00:58 -0500

    DB setup

--- backend/app/helpers/crawler.py
+++ backend/app/helpers/crawler.py
@@ -3,6 +3,18 @@ import requests
 from flask import Response
 from tokenizor import populate_keyword
 import json
+import pymongo
+
+mongoClient = pymongo.MongoClient("mongodb+srv://root:<pwd>@cluster0.uqeev.mongodb.net/<DBName>?retryWrites=true&w=majority")
+
+def databaseSetup():
+    dbList = mongoClient.list_database_names()
+    myDB = mongoClient['EasyConnectDB']
+    if myDB in dbList:
+        if 'ScholarList' not in myDB.list_collection_names():
+            myCol = myDB['ScholarList']
+
+    return myDB['ScholarList']
 
 def getAurthorObj(name):
     search_query = scholarly.search_author(name)
@@ -29,6 +41,8 @@ def getAllAbstract(pubList):
     return res
 
 if __name__ == "__main__":
+    collection = databaseSetup()
+    payload = []
     with open('researchersList.json', 'r') as f:
         data=json.load(f)
     for department in data:
@@ -36,7 +50,20 @@ if __name__ == "__main__":
             author = getAurthorObj(entry["name"])
             pub_list = getPublicationList(author)
             abs_val = getAllAbstract(pub_list)
+            #entry["name"]: [{title: key, keyword_list: keywords},...]
+            researcher = {}
+            researcher[entry["name"]] = []
             for key in abs_val:
+                res = {}
                 keywords = populate_keyword(abs_val[key])
-
+                res["title"] = key
+                res["keywords"] = keywords
+                researcher[entry["name"]].append(res)
+                break
+            break
+        payload.append(researcher)
+        break
+    with open('payload.txt', 'w+') as f:
+        f.write(str(payload))
+    collection.insert_many(payload)
         # TODO: update mongo DB with title, keyword list for prof list.
