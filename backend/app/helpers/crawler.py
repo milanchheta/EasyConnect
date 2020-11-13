6d7e332c130d53869bfc2b1302a1fdedf76473a4
milanchheta <milanchheta@gmail.com>
2020-11-12 23:56:33 -0500

    scholars api test done

--- backend/app/helpers/crawler.py
+++ backend/app/helpers/crawler.py
@@ -4,13 +4,38 @@ from bs4 import BeautifulSoup
 from flask import Response
 
 def getAurthorObj(name):
-    search_query = scholarly.search_author()
+    search_query = scholarly.search_author(name)
     author = next(search_query).fill()
     return author
 
-def getAbstract(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    results = soup.find('div', class_='abstractSection').get_text()
-    return results
+def getAuthorInterests(author):
+    return author.interests
+    
+def getPublicationList(author):
+    return author.publications
 
+def getAllAbstract(pubList):
+    res={}
+    for i in range(len(pubList)):
+        try:
+            pub = author.publications[i].fill().bib
+            res[pub["title"]]=pub["abstract"]
+            if i==20:
+                break
+        except:
+            i-=1
+            pass
+    return res
+
+
+# def getAbstract(url):
+#     page = requests.get(url)
+#     soup = BeautifulSoup(page.content, 'html.parser')
+#     results = soup.find('div', class_='abstractSection').get_text()
+#     ##add nlp algo here to extract imp words
+#     return results
+
+# author=getAurthorObj("Patrick Shih")
+# pubList=getPublicationList(author)
+# interests=getAuthorInterests(author)
+# absList=getAllAbstract(pubList)
