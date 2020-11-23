6b8087155ccfe6133355a7be550b49ba0ca01323
milanchheta <milanchheta@gmail.com>
2020-11-22 22:49:57 -0500

    new url

--- frontend/components/BASE_URL.js
+++ frontend/components/BASE_URL.js
@@ -1,2 +1,2 @@
-// export default BASE_URL = "https://7d1f142cb355.ngrok.io";
-export default BASE_URL = "http://10.0.2.2:5000";
+export default BASE_URL = "https://570a2daab842.ngrok.io";
+// export default BASE_URL = "http://10.0.2.2:5000";
